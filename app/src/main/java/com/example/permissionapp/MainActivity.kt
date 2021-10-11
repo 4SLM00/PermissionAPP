32892c4481bbb507653a43a8d93bcf9328727325
Sergi lona masriera <sergilmasriera@gmail.com>
2021-10-11 17:49:42 +0200

    finished

--- app/src/main/java/com/example/permissionapp/MainActivity.kt
+++ app/src/main/java/com/example/permissionapp/MainActivity.kt
@@ -1,27 +1,33 @@
 package com.example.permissionapp
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.ContextCompat
 import com.example.permissionapp.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    val REQUEST_LOCATION_CODE = 2
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        fusedLocationClient = LocationService.getFusedLocationProviderClient(this)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
     }
     private fun requestLocationPermission(){
         when{
             ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED -> {
                 getLastKnownLocation()
             }
-            else -> requestLocationPermission(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),REQUEST_LOCATION_CODE)
+            else -> requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),REQUEST_LOCATION_CODE)
         }
     }
 
@@ -41,8 +47,10 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
+@SuppressLint("MissingPermission")
     private fun getLastKnownLocation() {
-        fusedLocationClient.lastlocation
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location ->
+            location.let { it }
+        }
     }
 }
