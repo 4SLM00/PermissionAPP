8e0aaeee6d2ed93602b3fdb220558d4d7760280e
Sergi lona masriera <sergilmasriera@gmail.com>
2021-10-11 17:34:01 +0200

    logics added

--- app/src/main/java/com/example/permissionapp/MainActivity.kt
+++ app/src/main/java/com/example/permissionapp/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.example.permissionapp
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.example.permissionapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        fusedLocationClient = LocationService.getFusedLocationProviderClient(this)
+    }
+    private fun requestLocationPermission(){
+        when{
+            ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED -> {
+                getLastKnownLocation()
+            }
+            else -> requestLocationPermission(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),REQUEST_LOCATION_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            REQUEST_LOCATION_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults.first()==PackageManager.PERMISSION_GRANTED){
+                    getLastKnownLocation()
+                }
+            }
+        }
+    }
+
+
+
+    private fun getLastKnownLocation() {
+        fusedLocationClient.lastlocation
     }
 }
